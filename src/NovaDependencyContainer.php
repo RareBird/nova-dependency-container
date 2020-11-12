891a2a791227e15d03fa4686ce791f7abfaf94a8
Justin Dantzer <justin@rarebirdinc.com>
2020-11-12 09:56:20 -0500

    Add change from PR awaiting approval

--- src/NovaDependencyContainer.php
+++ src/NovaDependencyContainer.php
@@ -206,14 +206,15 @@ class NovaDependencyContainer extends Field
                     continue;
                 }
 
-                // @todo: quickfix for MorphTo
-                $morphable_attribute = $resource->getAttribute($dependency['property'].'_type');
-                if ($morphable_attribute !== null && Str::endsWith($morphable_attribute, '\\'.$dependency['value'])) {
-                    $this->meta['dependencies'][$index]['satisfied'] = true;
-                    continue;
+                if ($resource instanceof Resource) {
+                    // @todo: quickfix for MorphTo
+                    $morphable_attribute = $resource->getAttribute($dependency['property'].'_type');
+                    if ($morphable_attribute !== null && Str::endsWith($morphable_attribute, '\\'.$dependency['value'])) {
+                        $this->meta['dependencies'][$index]['satisfied'] = true;
+                        continue;
+                    }
                 }
             }
-
         }
     }
 
